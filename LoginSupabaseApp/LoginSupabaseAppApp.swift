6c29417dec231d7696f560ea7ab0acbe6c345c29
pouyasadri <pouyapaghandeh78@gmail.com>
2024-06-21 01:25:54 +0200

    Initial Commit

---
+++ LoginSupabaseApp/LoginSupabaseAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginSupabaseAppApp.swift
+//  LoginSupabaseApp
+//
+//  Created by Pouya Sadri on 21/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct LoginSupabaseAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
